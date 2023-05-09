7938e5cd9a9285621e7bd140422064e3d7fee3c0
Tamicktom <rickpokemon38@gmail.com>
2023-05-08 22:03:02 -0300

    add categorie creation

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { Router } from "express";
 import CreateUserController from "./controllers/user/CreateUserController";
 import DetailUserController from "./controllers/user/DetailUserController";
 import AuthUserController from "./controllers/user/AuthUserController";
+import CreateCategoryController from "./controllers/category/CreateCategoryController";
 import isAuthenticated from "./middlewares/isAuthenticated";
 
 const routes = Router();
@@ -12,5 +13,12 @@ const routes = Router();
 routes.post("/user", new CreateUserController().handle);
 routes.post("/session", new AuthUserController().handle);
 routes.get("/userinfo", isAuthenticated, new DetailUserController().handle);
+
+//* --------- Rotas para Category --------- *//
+routes.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { routes };
