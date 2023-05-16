75b44280f0990b80cb60226a937fea6807e34db0
Tamicktom <rickpokemon38@gmail.com>
2023-05-16 19:59:56 -0300

    Update variables.ts

--- src/variables.ts
+++ src/variables.ts
@@ -2,12 +2,20 @@
 import z from "zod";
 import dotenv from "dotenv";
 
-const { JWT_SECRET } = dotenv.config().parsed || {};
+//* Schemas
 const JWT_SECRET_SCHEMA = z.string().nonempty();
+const DATABASE_URL_SCHEMA = z.string().nonempty();
+
+//* Variables
+const { JWT_SECRET, DATABASE_URL } = dotenv.config().parsed || {};
+
+//* Validation
 if (!JWT_SECRET) throw new Error("Missing JWT_SECRET env variable");
+if (!DATABASE_URL) throw new Error("Missing DATABASE_URL env variable");
 
 const env = {
   JWT_SECRET: JWT_SECRET_SCHEMA.parse(JWT_SECRET),
+  DATABASE_URL: DATABASE_URL_SCHEMA.parse(DATABASE_URL),
 };
 
 export default env;
